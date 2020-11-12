b22aabda1a1ca50471329fa86e185296cea6a9c6
Lorenzo Balducci <lorenzo.balducci96@outlook.it>
2020-11-13 00:04:07 +0100

    inserted tyrant of halpi and the infernal crypt expansions

--- generateHTML.py
+++ generateHTML.py
@@ -3,19 +3,24 @@ import cv2
 import matplotlib.pyplot as plt
 import os
 
-baseFolder = "assets/tiles/rvA/"
-setName = "rvA"
+baseFolder = "assets/tiles/ciMarkers/"
+setName = "ciMarkers"
 pieceType = "tile"
-single = "yes"
+single = "no"
 snap = "yes"
 flippable = "no"
-input_path = 'C:/xampp/htdocs/wordpress/dungeon-saga-quest-builder/assets/tiles/rvA'
+onMapZIndex = "2"
+input_path = 'C:/xampp/htdocs/wordpress/dungeon-saga-quest-builder/assets/tiles/ciMarkers'
 output_path = 'C:/xampp/htdocs/wordpress/dungeon-saga-quest-builder/assets/tiles/output.txt'
-file = open(output_path,"w+") 
+file = open(output_path,"w") 
 for filename in os.listdir(input_path):
         image = filename.split('.')[0]
-        idName = image + "_1"
+        idName = ""
+        if(single == "yes"):
+                idName = image
+        else:
+                idName = image + "_1"
         file.write("<image src=\"" + baseFolder + filename + "\" set=\"" + setName +
-        "\" image=\"" + image + "\" flippable=\"" + flippable + "\" orientation=\"0\" single=\"" + single + "\"\
+        "\" image=\"" + image + "\" flippable=\"" + flippable + "\" orientation=\"0\" onMapZIndex=\"" + onMapZIndex + "\" single=\"" + single + "\"\
         pieceType=\"" + pieceType + "\" snap=\"" + snap + "\" oncontextmenu=\"return false;\" id=\"" + idName + "\"\
         style=\"position: absolute; cursor: move; width: 100%;\" onMap=\"no\"></image>\n")
