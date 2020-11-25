dd3bf629623e71588b3fe85c5dab4d58936ba118
Lorenzo Balducci <lorenzo.balducci96@outlook.it>
2020-11-25 23:13:06 +0100

    inserted LOI expansion All config in config.php file

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$wp_load = "../wp-load.php"; //the path where is placed wp-load for integration with wordpress
+$email_to = "admin@domain.com"; //the receiver of the projects created by the community
+?>
